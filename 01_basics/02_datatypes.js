04c41ce983022f17f8e1e10ade5a36d6e2007d5f
Sha-lvi23 <143952370+Sha-lvi23@users.noreply.github.com>
2024-10-01 05:24:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version 
+
+console.log(3
+    +
+    3); //code readability should be high
+
+
+//number  2 to power 53
+//bigint 
+//string " "
+
+//boolean true/false
+//  null standalone value
+
+//undefined
+
+//symbol=unique
+
+//object
+//console.log(typeof "Shalvi");   //gives datatype of the asked object
+//undefined apne me hi ek datatype hai
+//lekin null ek object hai
+
+
